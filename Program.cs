0d3d60f7f3b71860e8e38b41fdb89f32cc36df67
Gabriel Argôlo <120109359+argologabriel@users.noreply.github.com>
2024-08-17 14:18:12 -0300

    sync: implementação de um sistema que trabalha com smartphones (#1)
    * feat: implementação da classe abstrata Smartphone
    
    * feat: implementação da classe Iphone
    
    * feat: implementação da classe Nokia
    
    * test: implementação dos testes com as classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// DONE: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia1 = new Nokia(numero: "79 1234-5678", modelo: "Modelo 1", imei: "111111111", memoria: 128);
+nokia1.Ligar();
+nokia1.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone1 = new Iphone(numero: "11 8765-4321", modelo: "Modelo 2", imei: "222222222", memoria: 256);
+iphone1.Ligar();
+iphone1.InstalarAplicativo("Telegram");
